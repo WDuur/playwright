8522ae8fd49c08a4372e9a61442fc58bcca4bb73
WDuur <w.duursma@outlook.com>
2025-06-23 21:41:38 +0200

    SV-002-2 add manage cookies +optimalization

--- features/cookie-banner.feature
+++ features/cookie-banner.feature
@@ -2,19 +2,28 @@ Feature: Cookie banner interaction
   Scenario: Clear all cookies
     Given the user visits the homepage
     When all cookies are cleared
-    Then the cookie banner should be visible
-    
+    Then the cookie banner should be "visible"
  
   Scenario: User accepts cookies
     Given the user visits the homepage
-    Then the cookie banner should be visible
+    Then the cookie banner should be "visible" 
     When the user clicks the "Accepteren" button
-    Then the cookie banner should disappear
+    Then the cookie banner should be "not visible" 
     And the "cookieConsent" cookie should be set to "true"
 
   Scenario: User dont accepts cookies
     Given the user visits the homepage
-    Then the cookie banner should be visible
+    Then the cookie banner should be "visible" 
     When the user clicks the "Weigeren" button
-    Then the cookie banner should disappear
+    Then the cookie banner should be "not visible" 
     And the "cookieConsent" cookie should be set to "false"
+
+Scenario: User wants to manage cookies
+    Given the user visits the homepage
+    And the cookie banner should be "visible"
+    When the user clicks the "Cookies beheren" link
+ 
+    Then the cookie settings should be shown
+    When the user clicks the checkbox to accept the cookies
+    And the user clicks the "Bevestigen" button
+    Then the "cookieConsent" cookie should be set to "true"
--- features/steps/cookie-banner.steps.ts
+++ features/steps/cookie-banner.steps.ts
@@ -1,34 +1,56 @@
 import { Given, When, Then } from './fixtures';
 import { expect } from '@playwright/test';
 
-Given('the user visits the homepage', async ({page}) => {
-  await page.goto('http://sharevalue.nl'); 
+const COOKIE_BANNER_SELECTOR = '.cookie-banner';
+
+Given('the user visits the homepage', async ({ page }) => {
+  await page.goto('http://sharevalue.nl');
 });
 
-When('all cookies are cleared', async  ({page}) =>{
-    await page.context().clearCookies();
-  });
-
-Then('the cookie banner should be visible', async ({page}) => {
-  const banner = page.locator('.cookie-banner');
-  await expect(banner).toBeVisible();
+When('all cookies are cleared', async ({ page }) => {
+  await page.context().clearCookies();
 });
 
-When('the user clicks the {string} button', async ({page}, buttonLabel: string) => {
-  const banner = page.locator('.cookie-banner');
-  const acceptButton = page.getByRole('button', { name: buttonLabel });
-  console.log('Selected button to click: ',  acceptButton);
-  await acceptButton.click();
+Then('the cookie banner should be {string}', async ({ page }, visibility) => {
+  const banner = page.locator(COOKIE_BANNER_SELECTOR);
+  if (visibility === 'visible') {
+    await expect(banner).toBeVisible();
+  } else if (visibility === 'not visible') {
+    await expect(banner).not.toBeVisible();
+  } else {
+    throw new Error(`Unknown visibility state: ${visibility}`);
+  }
 });
 
-Then('the cookie banner should disappear', async ({page}) =>{
-  const banner = page.locator('.cookie-banner');
+When('the user clicks the {string} button', async ({ page }, buttonLabel: string) => {
+  const button = page.getByRole('button', { name: buttonLabel });
+  await button.click();
+});
+
+Then('the cookie banner should disappear', async ({ page }) => {
+  const banner = page.locator(COOKIE_BANNER_SELECTOR);
   await expect(banner).toBeHidden();
 });
 
-Then('the {string} cookie should be set to {string}', async ({page}, cookieName: string, value: string) => {
+Then('the {string} cookie should be set to {string}', async ({ page }, cookieName: string, value: string) => {
   const cookies = await page.context().cookies();
   const cookie = cookies.find(c => c.name === cookieName);
-  console.log('Cookie: ', cookie);
   expect(cookie?.value).toBe(value);
+});
+
+When('the user clicks the {string} link', async ({ page }, linkText: string) => {
+  const link = page.locator(`${COOKIE_BANNER_SELECTOR} p:nth-of-type(3)`);
+  await expect(link).toHaveText(linkText);
+  await link.click();
+});
+
+Then('the cookie settings should be shown', async ({ page }) => {
+  const title = page.locator(`${COOKIE_BANNER_SELECTOR} p:nth-of-type(1)`, { hasText: 'Cookies beheren' });
+  await expect(title).toHaveText('Cookies beheren');
+});
+
+When('the user clicks the checkbox to accept the cookies', async ({ page }) => {
+  const checkbox = page.locator(`${COOKIE_BANNER_SELECTOR} input[type="checkbox"]`);
+  await expect(checkbox).toHaveCount(1);
+  await checkbox.check();
 });
